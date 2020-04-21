d5cbb3d76b4f6b82c6069c945c50ae85001e9191
kachase <48436337+kachase@users.noreply.github.com>
2020-04-21 12:31:44 +0300

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,15 +1,15 @@
-//a program that prints out a fibonacci sequence given a number
+#a program that prints out a fibonacci sequence given a number
 
 
 def Fib(n):
  if n<0:
- print("invalid!)
+  print("invalid!")
  elif n==0:
- return 0
+  return 0
  elif n==1:
- return 1
+  return 1
  
  else :
- return Fib(n-1) + Fib(n-2)
+  return Fib(n-1) + Fib(n-2)
 
 print(Fib(11))
